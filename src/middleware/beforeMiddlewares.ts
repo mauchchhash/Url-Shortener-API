400216e1eee1a05a9c45af638d36f5cc2d5efa34
Mahim Anjum Uchchhwas <51711462+mauchchhash@users.noreply.github.com>
2025-03-07 21:01:04 +0600

    Formatted with prettier

--- src/middleware/beforeMiddlewares.ts
+++ src/middleware/beforeMiddlewares.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
 function logReqInfo(req: Request, _: Response, next: NextFunction) {
-  console.log("---------------------------------------------------------------------------------------------------------")
-  console.log(`${req.method} -- ${req.url}`)
+  console.log(
+    "---------------------------------------------------------------------------------------------------------",
+  );
+  console.log(`${req.method} -- ${req.url}`);
   next();
 }
 
