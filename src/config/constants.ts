3a6f7cf2582b8dff604128121a2c36257d025b25
Mahim Anjum Uchchhwas <51711462+mauchchhash@users.noreply.github.com>
2025-04-02 18:46:08 +0600

    Refactor: removed unused codes

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,9 @@
 const configConstants = {
   accessTokenValidityInMinutes: 5,
   refreshTokenValidityInDays: 30,
+
+  // For debugging:
+  // accessTokenValidityInMinutes: 0.1,
+  // refreshTokenValidityInDays: 1 / (24 * 600),
 };
 export default configConstants;
